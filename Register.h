9bff8fb395f535c93e38907c5570e5156d266976
Hussien-55 <143922451+Hussien-55@users.noreply.github.com>
2024-11-04 19:59:31 +0200

    Update Register.h

--- Register.h
+++ Register.h
@@ -1,4 +1,26 @@
-#ifndef REGISTER_H
-#define REGISTER_H
+#ifndef TASK4_REGISTER_H
+#define TASK4_REGISTER_H
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+class Register{
+private:
+    string reg[16];
+public:
+    Register(){
+        for(int i=0; i<16; i++){
+            reg[i]="00";
+        }
+    }
 
+    void setcell(int address, string value){
+        reg[address] = value;
+    }
+
+    string getcell(int address){
+        return reg[address];
+    }
+
+};
 #endif
