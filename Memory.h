f93bafd67c9966f1cee77ba4e55c2fc1b016d7a3
Hussien-55 <143922451+Hussien-55@users.noreply.github.com>
2024-11-04 19:58:22 +0200

    Update Memory.h

--- Memory.h
+++ Memory.h
@@ -1,4 +1,27 @@
-#ifndef MEMORY_H
-#define MEMORY_H
+#ifndef TASK4_MEMORY_H
+#define TASK4_MEMORY_H
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+class Memory{
+private:
+    string memory[256];
+public:
+    Memory(){
+        for(int i=0; i<256; i++){
+            memory[i]="00";
+        }
+    }
+
+    void setcell(int address, string value){
+        memory[address] = value;
+    }
+
+    string getcell(int address){
+        return memory[address];
+    }
+
+};
 
 #endif
