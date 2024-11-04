ba70578a1fa8889df0496fe3d5a0557406c376c5
hvmdy808 <163043982+hvmdy808@users.noreply.github.com>
2024-11-04 19:36:13 +0200

    Update CU.h

--- CU.h
+++ CU.h
@@ -1,4 +1,27 @@
-#ifndef CU_H
-#define CU_H
+#ifndef TASK4_CU_H
+#define TASK4_CU_H
+#include <vector>
+#include <string>
+#include <iostream>
+#include "Register.h"
+#include "Memory.h"
+#include "ALU.h"
+using namespace std;
+class CU {
+public:
+    void load1(int reg_i, int mem_i, Register& rege, Memory& memo){
+        rege.setcell(reg_i,memo.getcell(mem_i));
+    }
+
+    void load2(int reg_i, string val, Register& rege){
+        rege.setcell(reg_i, val);
+    }
+
+    void store(int reg_i, int mem_i, Register& rege, Memory& memo){
+        memo.setcell(mem_i,rege.getcell(reg_i));
+    }
+
+};
+
 
 #endif
